8eed67e87c2bc8a9478c4ab42863007ac10ab698
jberdum <53492555+jberdum@users.noreply.github.com>
2019-08-22 12:10:35 -0300

    Add files via upload

---
+++ L4_ex04/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    Qual pacote é mais pesado? Faça um programa que receba o
+    peso de dois pacotes e em seguida exiba apenas o peso do
+    pacote mais pesado.
+    */
+
+    float pesoPacote1, pesoPacote2;
+
+    printf("\nInforme o peso do primeiro pacote: ");
+    scanf("%f", &pesoPacote1);
+    printf("\nInforme o peso do segundo pacote: ");
+    scanf("%f", &pesoPacote2);
+
+    if(pesoPacote1 > pesoPacote2){
+        printf("\nO pacote 1 tem o peso de %0.2f\n", pesoPacote1);
+    } else {
+        printf("\nO pacote 2 tem o peso de %0.2f\n", pesoPacote2);
+    }
+    return 0;
+}
